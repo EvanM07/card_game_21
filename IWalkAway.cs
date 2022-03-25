aa8751954cb401d4a9ddb1600f1f4804c347667d
Evan Masters <92110978+EvanM07@users.noreply.github.com>
2022-03-25 13:16:26 -0700

    adding and changing code
    added an interface and and changed code

---
+++ IWalkAway.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwentyOne
+{
+    interface IWalkAway
+    {
+        public void WalkAway(Player player);
+
+    }
+}
