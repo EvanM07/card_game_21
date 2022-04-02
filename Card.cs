86d2867a4745a46f928b9f202462519c1f11734a
Evan Masters <92110978+EvanM07@users.noreply.github.com>
2022-04-01 20:07:50 -0700

    Final Commit
    This will be the final commit for the card_game_21.

--- Card.cs
+++ Card.cs
@@ -8,13 +8,38 @@ namespace TwentyOne
 {
     public class Card
     {
-        //// this is the constructor. it gives these values to any object created using this class by default constructors always goes on top
-        //public Card()
-        //{
-        //    Suit = "Spades";
-        //    Face = "two";
-        //}
-        public string Suit { get; set; }
-        public string Face { get; set;  }
+        public Suit Suit { get; set; }
+        public Face Face { get; set;  }
+
+        public override string ToString()
+        {
+            return string.Format("{ 0} of {1}", Face, Suit);
+
+            
+        }
+    }
+    public enum Suit
+    {
+        Clubs,
+        Diamonds,
+        Hearts,
+        Spades
+    }
+
+    public enum Face
+    {
+        Two,
+        Three,
+        Four,
+        Five,
+        Six,
+        Seven,
+        Eight,
+        Nine,
+        Ten,
+        Jack,
+        Queen,
+        King, 
+        Ace
     }
 }
